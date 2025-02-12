8679f84efde5a23a4ac142e52787538b04b151c1
Marco De Tommasi <79840407+MarcoDeTommasi@users.noreply.github.com>
2025-02-12 16:00:55 +0100

    to do, config per MacOS

--- test.py
+++ test.py
@@ -1,51 +1,61 @@
-from transformers import AutoModelForCausalLM
+from langchain_community.chat_models import ChatOllama
 
-from deepseek_vl.models import DeepseekVLV2Processor, DeepseekVLV2ForCausalLM
-from deepseek_vl.utils.io import load_pil_images
+#doc-intelligence like per valutare meglio pdf
+#collegamento dict creato con dict lista nuova
+#acquisizione utente e salvataggio in un db per memoria
 
 
-# specify the path to the model
-model_path = "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B"
-vl_chat_processor: DeepseekVLV2Processor = DeepseekVLV2Processor.from_pretrained(model_path)
-tokenizer = vl_chat_processor.tokenizer
+llm = ChatOllama(model='llama3', temperature=0.01)
 
-vl_gpt: DeepseekVLV2ForCausalLM = AutoModelForCausalLM.from_pretrained(model_path, trust_remote_code=True)
-vl_gpt = vl_gpt.to(torch.bfloat16).cuda().eval()
 
-## single image conversation example
-conversation = [
-    {
-        "role": "<|User|>",
-        "content": "<image>\n<|ref|>The giraffe at the back.<|/ref|>.",
-        "images": ["./images/visual_grounding.jpeg"],
-    },
-    {"role": "<|Assistant|>", "content": ""},
+template = """Ti darò un piano alimentare personalizzato per un utente. In questo piano sono presenti le linee guida alimentari per ogni giorno della settimana e per ogni pasto.
+Ti chiederò cosa è consigliato mangiare per un dato pasto in un dato giorno e tu devi rispondere elencanto i cibi e le quantità riportate.
+
+Ogni giorno ha questi possibili pasti:
+
+['Colazione','Spuntino','Pranzo','Cena']
+
+NOTA: riportami 2 spuntini, uno mattutino e uno pomeridiano, 
+se ti chiedo di parlare di uno spuntino e aggiungo mattutino o pomeridiano riportami rispettivamente quello fra la colazione e il pranzo e quello fra il pranzo e la cena
+
+NOTA: riportami la risposta come una lista Python.
+
+DEVI Rispettare questo formato, la quantità e sempre riportata:
+[ 'alimento1 quantita', 'alimento2 quantita', 'alimento3 quantita']
+
+Riporta solo il tipo di alimenti e la quatità, evita aggettivi:
+
+Es. Latte parzialmente scremato 200ml -> Latte 200ml
+Es. Yogurt Greco 150g-> Yogurt 150g
+
+Tralascia tutti gli aggettivi
+
+se come unità di misura compaiono "g" "ml" riportale, in caso di assenza utilizza "pz"
+Le unità di misura non devono comparire fra parentesi
+
+riportami solo i cibi relativi al pasto indicato.
+
+IMPORTANTE: EVITA LE ALTERNATIVE
+
+
+NOTA: NON FORNIRE ALTRO TESTO OLTRE ALLA LISTA
+
+{context}
+
+Question: {question}
+"""
+
+
+custom_prompt = template.format(context=template, question="question")
+
+# Inizializza il modello LLM
+llm = ChatOllama(model='llama3', temperature=0.01)
+
+# Utilizza il metodo `invoke` per interrogare il modello
+messages = [
+    ("system", "You are a helpful assistant. Use the given context to answer questions accurately."),
+    ("human", custom_prompt)
 ]
 
-
-# load images and prepare for inputs
-pil_images = load_pil_images(conversation)
-prepare_inputs = vl_chat_processor(
-    conversations=conversation,
-    images=pil_images,
-    force_batchify=True,
-    system_prompt=""
-).to(vl_gpt.device)
-
-# run image encoder to get the image embeddings
-inputs_embeds = vl_gpt.prepare_inputs_embeds(**prepare_inputs)
-
-# run the model to get the response
-outputs = vl_gpt.language_model.generate(
-    inputs_embeds=inputs_embeds,
-    attention_mask=prepare_inputs.attention_mask,
-    pad_token_id=tokenizer.eos_token_id,
-    bos_token_id=tokenizer.bos_token_id,
-    eos_token_id=tokenizer.eos_token_id,
-    max_new_tokens=512,
-    do_sample=False,
-    use_cache=True
-)
-
-answer = tokenizer.decode(outputs[0].cpu().tolist(), skip_special_tokens=True)
-print(f"{prepare_inputs['sft_format'][0]}", answer)
+response = llm.invoke(messages)
+answer = response.content.strip()
