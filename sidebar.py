958cd3a06f56dcc5386956d7f0d5694140c0df40
Marco De Tommasi <md039@rina.org>
2025-02-17 10:33:41 +0100

    sidebar fix

--- sidebar.py
+++ sidebar.py
@@ -1,22 +1,23 @@
 import streamlit as st
 
 def mostra_sidebar():
-    if 'username' not in st.session_state:
-        st.session_state.username = None
-        st.session_state.nome = None
-        st.session_state.cognome = None
-        st.session_state.pagina_corrente = 'home'
+    if "authentication_status" in st.session_state.keys() and st.session_state["authentication_status"]:
+        if 'username' not in st.session_state:
+            st.session_state.username = None
+            st.session_state.nome = None
+            st.session_state.cognome = None
+            st.session_state.pagina_corrente = 'home'
 
-    with st.sidebar:
-        if st.session_state.nome is not None and st.session_state.cognome is not None:
-            st.write(f"### Ciao, {st.session_state.nome} {st.session_state.cognome}! ")
-        st.write(f"Username: {st.session_state.username}")
-        st.write(f"Pagina corrente: {st.session_state.pagina_corrente}")
+        with st.sidebar:
+            if st.session_state.nome is not None and st.session_state.cognome is not None:
+                st.write(f"### Ciao, {st.session_state.nome} {st.session_state.cognome}! ")
+            st.write(f"Username: {st.session_state.username}")
+            st.write(f"Pagina corrente: {st.session_state.pagina_corrente}")
 
-        if st.button("Logout 🚪"):
-            if "authentication_status" in st.session_state.keys() and st.session_state["authentication_status"]:
-                for key in list(st.session_state.keys()):
-                    del st.session_state[key]
-                st.switch_page('pages/1_home.py')
-            else:
-                st.switch_page('0_login.py')
+            if st.button("Logout 🚪"):
+                if "authentication_status" in st.session_state.keys() and st.session_state["authentication_status"]:
+                    for key in list(st.session_state.keys()):
+                        del st.session_state[key]
+                    st.switch_page('pages/1_home.py')
+                else:
+                    st.switch_page('0_login.py')
